97961c012b56d903e57b5ca6ce7b96a299daa61f
DimmyJing <jimmyguding@gmail.com>
2020-07-14 11:12:47 -0500

    implemented api requests

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -18,22 +18,31 @@ export default function(props: {extraInfoHandler: (extraInfo: any) => void, curr
         longitude: -96.788420,
         zoom: 13,
     });
+    let [prevViewport, setPrevViewport] = useState(viewport);
+    const [places, setPlaces] = useState([] as [number, number, string, string][]);
+    const [term, setTerm] = useState("");
 
-    const maxMarkerNum = 100;
+    if (viewport !== prevViewport || term !== currentTerm) {
+        setPrevViewport(viewport);
+        setTerm(currentTerm);
+        const maxMarkerNum = 100;
 
-    const locations = GetLocations(currentTerm, viewport.longitude, viewport.latitude);
+        const mercatorViewport = new WebMercatorViewport(viewport);
+        const bottomLeft = mercatorViewport.unproject([0, 0]);
+        const topRight = mercatorViewport.unproject([mercatorViewport.width, mercatorViewport.height]);
+        const leftX = bottomLeft[0];
+        const rightX = topRight[0];
+        const bottomY = topRight[1];
+        const topY = bottomLeft[1];
 
-    const mercatorViewport = new WebMercatorViewport(viewport);
-    const bottomLeft = mercatorViewport.unproject([0, 0]);
-    const topRight = mercatorViewport.unproject([mercatorViewport.width, mercatorViewport.height]);
-    const leftX = bottomLeft[0];
-    const rightX = topRight[0];
-    const bottomY = topRight[1];
-    const topY = bottomLeft[1];
-
-    let filteredLocations = locations.filter(el => el[0] >= leftX && el[0] <= rightX && el[1] >= bottomY && el[1] <= topY);
-    if (filteredLocations.length > maxMarkerNum) {
-        filteredLocations = filteredLocations.splice(0, maxMarkerNum);
+        const locations = GetLocations(currentTerm, viewport.longitude, viewport.latitude);
+        locations.then(data => {
+            let filteredLocations = data.filter(el => el[0] >= leftX && el[0] <= rightX && el[1] >= bottomY && el[1] <= topY);
+            if (filteredLocations.length > maxMarkerNum) {
+                filteredLocations = filteredLocations.splice(0, maxMarkerNum);
+            }
+            setPlaces(filteredLocations);
+        });
     }
 
     return (
@@ -41,7 +50,7 @@ export default function(props: {extraInfoHandler: (extraInfo: any) => void, curr
             <ReactMapGL width="100vw" height="100vh" {...viewport}
              onViewportChange={viewport => setViewport(viewport)}
              mapStyle="mapbox://styles/aephus/ckckyntll05jg1io32lfhe18d">
-                <Markers data={filteredLocations} extraInfoHandler={extraInfoHandler}/>
+                <Markers data={places} extraInfoHandler={extraInfoHandler}/>
             </ReactMapGL>
         </Container>
     );
--- src/core/GetLocations.ts
+++ src/core/GetLocations.ts
@@ -27,24 +27,40 @@ function get_testing_location() {
     }));
 }
 
-function get_api_call(loc_name: string, longitude: number, latitude: number) {
-    return [[-96.79480599999999, 33.065467, "LifeCare Hospital of Plano", ""],
-            [-96.8228893, 33.083381, "Community Hospital Corporation", ""],
-            [-96.7708937, 33.1142743, "Baylor Scott & White Medical Center - Centennial", ""],
-            [-96.8379875, 33.1046353, "Baylor Scott & White Medical Center - Frisco", ""],
-            [-96.8006283, 33.0787498, "Children's Medical Center Plano", ""],
-            [-96.8359763, 33.0440114, "Texas Health Presbyterian Hospital Plano", ""],
-            [-96.7698309, 33.1136626, "Baylor Hospital", ""]] as [number, number, string, string][]
+async function get_api_call(loc_name: string, longitude: number, latitude: number) {
+    const google = (window as any).google;
+    const service = new google.maps.places.PlacesService(document.createElement('div'));
+    const pos = new google.maps.LatLng(latitude, longitude);
+    console.log("QUERYING " + loc_name)
+    const parameter = {location: pos, radius: '5000', query: loc_name};
+    const nearbySearch = (parameter: object): Promise<[number, number, string, string][]> => {
+        return new Promise((resolve: any, reject: any) => {
+            service.textSearch(parameter, (results: any, status: any) => {
+                if (status !== google.maps.places.PlacesServiceStatus.OK) {
+                    return reject(status);
+                }
+                const result: [number, number, string, string][] = [];
+                for (const i of results) {
+                    const latlng = [i.geometry.location.lat(), i.geometry.location.lng()];
+                    const name = i.name;
+                    const address = "Address: " + i.vicinity;
+                    result.push([latlng[1], latlng[0], name, address])
+                }
+                resolve(result);
+            })
+        })
+    };
+    return nearbySearch(parameter);
 }
 
-export default function(loc_name: string, longitude: number, latitude: number) {
+export default async function(loc_name: string, longitude: number, latitude: number) {
     if (cached_data[loc_name] !== undefined) {
         return cached_data[loc_name] as [number, number, string, string][];
     }
     if (loc_name === "Testing Locations") {
         cached_data[loc_name] = get_testing_location();
     } else {
-        cached_data[loc_name] = get_api_call(loc_name, longitude, latitude);
+        cached_data[loc_name] = await get_api_call(loc_name, longitude, latitude);
     }
     return cached_data[loc_name] as [number, number, string, string][];
 }
